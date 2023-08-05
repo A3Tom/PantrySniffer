a6903018748e15757276f0c1582244f20bbb3665
A3Tom <Tom_Esq@Yahoo.com>
2023-08-05 23:53:53 +0100

    Cookin. Wae. Gas. got a function on the go n it's firin hings aff. Wit a time

---
+++ Services/PantrySnifferServices/PS.IngredientService.Functions/Endpoints/IngredientFunctions.cs
@@ -0,0 +1,93 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using PS.Common.Domain.Enums;
+using PS.Common.Queries.Ingredient;
+using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace PS.IngredientService.Functions.Endpoints;
+
+public class IngredientFunctions
+{
+    private readonly IMediator _mediator;
+
+    public IngredientFunctions(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet("Search")]
+    [FunctionName(nameof(Search))]
+    public async Task<IActionResult> Search(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "POST")] SearchIngredientsQuery request,
+        ILogger log)
+    {
+        log.LogInformation($"{nameof(Search)} triggered with {request.SearchTerm}");
+
+        var result = await _mediator.Send(request);
+
+        return new OkObjectResult($"{JsonSerializer.Serialize(result)}");
+    }
+
+    [FunctionName(nameof(Get))]
+    public async Task<IActionResult> Get(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "GET", Route = "{ingredientId}")] HttpRequestMessage request,
+        Guid ingredientId,
+        ILogger log)
+    {
+        log.LogInformation($"{nameof(Get)} triggered with {ingredientId}");
+
+        var result = await _mediator.Send(new GetIngredientByIdQuery(ingredientId));
+
+        return new OkObjectResult($"{JsonSerializer.Serialize(result)}");
+    }
+
+    [FunctionName(nameof(Create))]
+    public async Task<IActionResult> Create(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "POST")] UpsertIngredientCommand request,
+        ILogger log)
+    {
+        log.LogInformation($"{nameof(Create)} triggered with {request.Id}");
+
+        var entityAlreadyExists = await _mediator.Send(new GetIngredientByNameQuery(request.Name));
+
+        if (entityAlreadyExists != null)
+            return new BadRequestObjectResult($"Object already exists with name {request.Name} ({entityAlreadyExists.Id})");
+
+        var result = await _mediator.Send(request);
+
+        return new OkObjectResult($"{JsonSerializer.Serialize(result)}");
+    }
+
+    [FunctionName(nameof(Update))]
+    public async Task<IActionResult> Update(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "PUT")] UpsertIngredientCommand request,
+        ILogger log)
+    {
+        log.LogInformation($"{nameof(Create)} triggered with {request.Id}");
+
+        var result = await _mediator.Send(request);
+
+        return new OkObjectResult($"{JsonSerializer.Serialize(result)}");
+    }
+
+    [FunctionName(nameof(ListByType))]
+    public async Task<IActionResult> ListByType(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "GET", Route = "ListByType/{ingredientTypeInt:int?}")] HttpRequestMessage request,
+        int ingredientTypeInt,
+        ILogger log)
+    {
+        var ingredientType = (IngredientType)ingredientTypeInt;
+        log.LogInformation($"{nameof(Search)} triggered with {ingredientType} ({Enum.GetName(ingredientType)})");
+
+        var result = await _mediator.Send(new ListIngredientsByTypeQuery(ingredientType));
+
+        return new OkObjectResult($"{JsonSerializer.Serialize(result)}");
+    }
+}
---
+++ Services/PantrySnifferServices/PS.IngredientService.Functions/Startup.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PS.Common.Domain.Extensions;
+using PS.Common.Domain.Settings;
+using PS.Common.Queries.Ingredient;
+using PS.IngredientService.Application.Handlers.Queries;
+using PS.IngredientService.Data.Repositories;
+using PS.IngredientService.Functions;
+using System;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace PS.IngredientService.Functions;
+internal class Startup : FunctionsStartup
+{
+    public override void Configure(IFunctionsHostBuilder builder)
+    {
+        ConfigureSettings(builder.Services);
+        ConfigureLogging(builder.Services);
+        ConfigureMediator(builder.Services);
+
+        ConfigureServiceLifetimeScopes(builder.Services);
+    }
+
+    private static void ConfigureSettings(IServiceCollection services)
+    {
+        var appConfig = services.BuildConfigurationRoot();
+        var settings = appConfig.Get<PantrySettings>();
+
+        if (settings == null)
+            throw new NullReferenceException("Cannot instantiate settings, please check your Azure application config or local.settings.json file");
+
+        settings.AddToServiceConfiguration(services);
+    }
+
+    private static void ConfigureMediator(IServiceCollection services)
+    {
+        services.AddMediatR(options => options.RegisterServicesFromAssemblyContaining<SearchIngredientsQuery>());
+        services.AddMediatR(options => options.RegisterServicesFromAssemblyContaining<SearchIngredientsHandler>());
+        services.AddMediatR(options => options.RegisterServicesFromAssemblyContaining<UpsertIngredientCommand>());
+    }
+
+    private static void ConfigureLogging(IServiceCollection services)
+    {
+        services.AddLogging();
+    }
+
+    private static void ConfigureServiceLifetimeScopes(IServiceCollection services)
+    {
+        services.AddTransient<IIngredientsRepository, IngredientsRepository>();
+    }
+}
