9aae952513c9c4e96f79ccfbeb0fbd1a8bc924d6
A3Tom <Tom_Esq@Yahoo.com>
2023-08-05 23:49:43 +0100

    Added settings

---
+++ Services/PantrySnifferServices/PS.Common.Domain/Settings/Abstract/ICosmosDbSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace PS.Common.Domain.Settings.Abstract;
+public interface ICosmosDbSettings
+{
+    string CosmosDatabaseName { get; init; }
+    string CosmosContainerName { get; init; }
+    string CosmosConnectionString { get; init; }
+}
---
+++ Services/PantrySnifferServices/PS.Common.Domain/Settings/PantrySettings.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PS.Common.Domain.Settings.Abstract;
+
+namespace PS.Common.Domain.Settings;
+public class PantrySettings : ICosmosDbSettings
+{
+    public string CosmosDatabaseName { get; init; } = string.Empty;
+    public string CosmosContainerName { get; init; } = string.Empty;
+    public string CosmosConnectionString { get; init; } = string.Empty;
+
+    public void AddToServiceConfiguration(IServiceCollection services)
+    {
+        services.AddSingleton<ICosmosDbSettings>(this);
+    }
+}
